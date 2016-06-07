ac981c4662704f1570923daa529b53d277d7da41
Matthew-Mitchell <matthewbmitchell@gmail.com>
2016-06-06 20:07:04 -0400

    Update advanced_python_regex.py
    Rough draft of response. Code is slightly brittle in terms of various formatting errors/typos it accounts for. Still need to count types of degrees etc.

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,11 @@
-PLACE YOUR CODE HERE
+import csv
+d=dict()
+with open('faculty.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        row[1]=row[1].strip()
+        degrees=row[1].split()
+        for degree in degrees:
+            degree=degree.replace(".","")
+            d[degree]=d.get(degree,0)+1
+print d
