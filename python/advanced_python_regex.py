68cd7b4f59dd37547e5d4fd05b813e726f8acfae
Matthew-Mitchell <matthewbmitchell@gmail.com>
2016-06-07 21:26:45 -0400

    Update advanced_python_regex.py
    Q1-4 Answered. Still feel like I could use additional practice implementing various methods and interchanging object types etc. Would like to create a histogram of the results before the weekend.

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,17 +1,31 @@
 import csv
-d=dict()
-with open('faculty.csv') as csvfile:
-    reader = csv.reader(csvfile)
+import re
+count_title=dict()
+emailist=[]
+domainlist=[]
+domaincount=dict()
+count_degrees=dict()
+
+with open('faculty.csv', 'rb') as f:
+    reader = csv.DictReader(f)
     for row in reader:
-        row[1]=row[1].strip()
-        degrees=row[1].split()
-        for degree in degrees:
-            degree=degree.replace(".","")
-            d[degree]=d.get(degree,0)+1
-print d
+        row[' degree']=row[' degree'].strip()
+        row[' degree']=row[' degree'].replace(".","")
+        degreelist=row[' degree'].split(" ")
+        count_title[(row[' title'])]=count_title.get(row[' title'],0)+1
+        for degree in degreelist:
+            count_degrees[degree]=count_degrees.get(degree,0)+1
+        emailist.append(row[' email'])
+        
+print "Q1:\n\n", count_degrees, "\n\n"        
+print "Q2:\n\n", count_title, "\n\n"
+print "Q3:\n\n", emailist, "\n\n"
 
 
-#Need an alternative method for dealing with multiple degrees.
-#Multiple for loops are to be avoided.
-#Current thoughts: which csv read method will be best?
-#Should I incorporate regular expressions or simply reformat using string methods?
+for address in emailist:
+    domainlist.append(re.findall('@(\S+)', address))
+
+for domain in domainlist:
+    domain=str(domain)
+    domaincount[domain]=domaincount.get(domain,0)+1
+print "Q4:\n\n", domaincount
