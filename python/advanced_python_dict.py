1478dd8043c5ca21d4baf87f10d727b7d38617bd
Matthew-Mitchell <matthewbmitchell@gmail.com>
2016-06-10 11:21:30 -0400

    Update advanced_python_dict.py
    Update with Q6

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,16 @@
-PLACE YOUR CODE HERE
+import csv
+import re
+directory=dict()
+rowdata=[]
+
+with open('faculty.csv', newline='') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        rowdata.append(row)
+for row in rowdata[1:]:
+    try:
+        directory[row[0].split()[-1]]=directory.get(row[0].split()[-1])+[row[1:]]
+    except:
+        directory[row[0].split()[-1]]=[row[1:]]
+print(directory)
+#Written in Python 3; to convert to Python 2 change 2nd argument ins open from newline='' to 'rb'
