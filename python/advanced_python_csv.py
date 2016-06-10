01376bb3f3c1d1a9fcd67fa2ddead272f6cb2918
Matthew-Mitchell <matthewbmitchell@gmail.com>
2016-06-10 16:22:15 -0400

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,12 @@
-PLACE YOUR CODE HERE
+import csv
+emailist=[]
+
+with open('faculty.csv', 'rb') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        emailist.append(row[' email'])
+        
+with open('emails.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
+    for row in emailist:
+        writer.writerow(row)
