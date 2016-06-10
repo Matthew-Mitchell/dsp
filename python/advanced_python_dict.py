90134dd71eaf91a48cfc93a57d3b7bc0422524c6
Matthew-Mitchell <matthewbmitchell@gmail.com>
2016-06-10 12:23:34 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -33,5 +33,25 @@ for row in rowdata[1:]:
         directory[firstlast]=[row[1:]]
 print(directory)
 
+#Q8
+import csv
+import re
+directory=dict()
+rowdata=[]
 
+with open('faculty.csv', newline='') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        rowdata.append(row)
+for row in rowdata[1:]:
+    firstlast=row[0].split()[0],row[0].split()[-1]
+    try:
+        directory[firstlast]=directory.get(firstlast)+[row[1:]]
+    except:
+        directory[firstlast]=[row[1:]]
+directorykeys=sorted(directory.keys(),key=lambda x: x[-1])
+directoryvalues=[directory[x] for x in directorysort]
+directorybook=zip(directorykeys,directoryvalues)
+for person in directorybook:
+    print(person)
 #Written in Python 3; to convert to Python 2 change 2nd argument ins open from newline='' to 'rb'
