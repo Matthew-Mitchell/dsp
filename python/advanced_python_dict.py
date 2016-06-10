29dceae4017fcc44432492dd505f9b71b323cc08
Matthew-Mitchell <matthewbmitchell@gmail.com>
2016-06-10 11:37:27 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,3 +1,4 @@
+#Question 6
 import csv
 import re
 directory=dict()
@@ -13,4 +14,24 @@ for row in rowdata[1:]:
     except:
         directory[row[0].split()[-1]]=[row[1:]]
 print(directory)
+
+#Question 7
+import csv
+import re
+directory=dict()
+rowdata=[]
+
+with open('faculty.csv', newline='') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        rowdata.append(row)
+for row in rowdata[1:]:
+    firstlast=row[0].split()[0],row[0].split()[-1]
+    try:
+        directory[firstlast]=directory.get(firstlast)+[row[1:]]
+    except:
+        directory[firstlast]=[row[1:]]
+print(directory)
+
+
 #Written in Python 3; to convert to Python 2 change 2nd argument ins open from newline='' to 'rb'
